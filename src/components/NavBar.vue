44bfa210f70f3c3e45017dd53688185368a7e1d3
Emmanuel Yapura <eyapura96@gmail.com>
2025-10-23 17:43:03 -0300

    modifica estilos componente navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,16 @@
 <template>
-    <nav class="navbar">
-        <ul>
-            <li v-for="nav in navegacion" :key="nav.nombre"><a :href="nav.enlace" class="nav-item" >{{ nav.nombre }} </a></li>
-        </ul>
-    </nav>
+    <header>
+        <div class="header-content">
+
+            <h3> {{ "< Portafolio />" }}</h3>
+            <nav class="navbar">
+                <ul>
+                    <li v-for="nav in navegacion" :key="nav.nombre"><a :href="nav.enlace" class="nav-item" >{{ nav.nombre }} </a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    </header>
 </template>
 
 <script setup>
@@ -17,6 +24,23 @@ const navegacion = ref([
 </script>
 
 <style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    margin: 0;
+    background-color: #101821;
+
+    h3{
+        color: #EEEEEE;
+        padding-left: 2rem;
+    }
+}
+
 ul{
     display: flex;
     justify-content: space-around;
@@ -25,18 +49,28 @@ ul{
 
 li{
     list-style: none;
+    padding-right: 1.2rem;
 }
-/* Esta clase se puede poner en todos los contenedores hijos de app! o para una clase especial solo para centrar en desk */
-.navbar{
-    max-width: 1280px;
-    margin: 0 auto;
+
+.header-content {
+    width: 100%;   
+    max-width: 1420px;    
+    margin: 0 auto;    
+    padding: 1.8rem 0;   
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     border: 1px solid black;
-    padding: 1rem;
+}
+
+.navbar{
+    padding-right: 0.5rem;
 }
 
 .nav-item{
     text-decoration: none;
-    color: black;
+    color: #EEEEEE;
 }
 
 @media (min-width: 992px ){
@@ -47,6 +81,22 @@ ul{
 .nav-item{
     font-size: 1.15rem;
 } 
+}
+
+@media (max-width: 545px) {
+    
+    header h3{
+        padding-left: 0;
+        margin-bottom: 1rem;
+    }
+    
+    .header-content{
+        flex-direction: column;
+    }
+
+    .navbar{
+        padding-right: 0;
+    }
 }
 
 
