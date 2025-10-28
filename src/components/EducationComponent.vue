5c45cea21187ad1d17fd9f51747c049960577173
Emmanuel Yapura <eyapura96@gmail.com>
2025-10-28 01:50:06 -0300

    modifica fondo de los articulos

--- src/components/EducationComponent.vue
+++ src/components/EducationComponent.vue
@@ -7,8 +7,7 @@
                     <div class="title-cards">
                         <h3> {{ curso.nombre }}</h3> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-school"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" /><path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" /></svg>
                     </div>
-                        
-                        <p>{{ curso.descripcion }} - {{ curso.fecha }}</p>
+                    <p>{{ curso.descripcion }} - {{ curso.fecha }}</p>
                 </article>
             </div>
         </div>
@@ -60,6 +59,7 @@ const cursos = ref([
         padding: 1.5rem;
         max-width: 520px;
         color: #DDDDDD;
+        background-color: #2a2f3a;
         border-radius: 15px;
         
         .title-cards{
